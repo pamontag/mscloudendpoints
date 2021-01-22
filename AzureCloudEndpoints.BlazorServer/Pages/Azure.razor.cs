2e1a7cccae62ba816e244e5e6b45d178439ea1df
Paolo Montagna <pamontag@microsoft.com>
2021-01-22 11:05:28 +0100

    Separated code from page

---
+++ AzureCloudEndpoints.BlazorServer/Pages/Azure.razor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureCloudEndpoints.BlazorServer.Model;
+using AzureCloudEndpoints.BlazorServer.Data;
+
+namespace AzureCloudEndpoints.BlazorServer.Pages
+{
+    public partial class Azure : ComponentBase
+    {
+        [Inject]
+        public EndpointService endpointService { get; set; }
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        public AzureEndpoint[] Endpoints { get; set; }
+        public AzureEndpoint[] SelectedEndpoints { get; set; }
+        public string[] Services { get; set; }
+        public string SelectedService { get; set; }
+        public string[] Regions { get; set; }
+        public string SelectedRegion { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Endpoints = (await endpointService.GetAzureEndpointsAsync()).values.ToArray();
+            Endpoints.Where(e => e.properties.region == "").ToList().ForEach(e => e.properties.region = "global");
+            Endpoints.Where(e => e.properties.systemService == "").ToList().ForEach(e => e.properties.systemService = "AzureCloud");
+            Services = Endpoints.Select(e => e.properties.systemService).Distinct().OrderBy(e => e).ToArray();
+            SelectedService = Services.Single(r => r == "AzureCloud");
+            Regions = Endpoints.Select(e => e.properties.region).Distinct().OrderBy(e => e).ToArray();
+            SelectedRegion = Regions.Single(r => r == "westeurope");
+            SelectedEndpoints = Endpoints.Where(e => e.properties.systemService == SelectedService && e.properties.region == SelectedRegion).ToArray();
+        }
+
+        public void OnChangeService(string service)
+        {
+            SelectedService = service;
+            SelectedEndpoints = Endpoints.Where(e => e.properties.systemService == SelectedService && e.properties.region == SelectedRegion).ToArray();
+            if (SelectedEndpoints.Count() == 0)
+            {
+                OnChangeRegion("global");
+            }
+        }
+
+        public void OnChangeRegion(string region)
+        {
+            SelectedRegion = region;
+            SelectedEndpoints = Endpoints.Where(e => e.properties.systemService == SelectedService && e.properties.region == SelectedRegion).ToArray();
+        }
+
+        void OnClickDownloadButton()
+        {
+            navigationManager.NavigateTo($"api/download/{SelectedService}/{SelectedRegion}", true);
+
+        }
+    }
+}
---
+++ AzureCloudEndpoints.BlazorServer/Pages/Office365.razor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureCloudEndpoints.BlazorServer.Data;
+using Microsoft.AspNetCore.Components;
+using AzureCloudEndpoints.BlazorServer.Model;
+
+namespace AzureCloudEndpoints.BlazorServer.Pages
+{
+    public partial class Office365 : ComponentBase
+    {
+        [Inject]
+        public EndpointService endpointService { get; set; }
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        public Office365Endpoint[] Endpoints { get; set; }
+        public Office365Endpoint[] SelectedEndpoints { get; set; }
+        public string[] Services { get; set; }
+        public string SelectedService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            SelectedEndpoints = Endpoints = await endpointService.GetOffice365EndpointsAsync();
+            Services = Endpoints.Select(e => e.serviceArea).Distinct().ToArray();
+            SelectedService = Services.First();
+            SelectedEndpoints = Endpoints.Where(e => e.serviceArea == SelectedService).ToArray();
+        }
+
+        void OnChangeService(string service)
+        {
+            SelectedService = service;
+            SelectedEndpoints = Endpoints.Where(e => e.serviceArea == SelectedService).ToArray();
+        }
+
+        void OnClickDownloadButton()
+        {
+            navigationManager.NavigateTo($"api/download/{SelectedService}", true);
+
+        }
+    }
+}
